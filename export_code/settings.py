ce79b4fedc24de861711b36615c2ef89274cbd06
Tomasuh <3432107+Tomasuh@users.noreply.github.com>
2019-11-03 15:11:12 +0100

    Create settings.py

---
+++ export_code/settings.py
@@ -0,0 +1,9 @@
+# Emercoin settings
+e_url = "http://127.0.0.1:6662/"
+e_rpc_user = ""
+e_rpc_password = ""
+
+# Namecoin settings
+n_url = "http://127.0.0.1:6663/"
+n_rpc_user = ""
+n_rpc_password = ""
